fedcb4c76124e820fd89699381ad16f5bd4a89aa
Maxwell Knight <maxwell.knight98@gmail.com>
2024-01-18 15:52:11 +0200

    renamed variables in kruskal algorithm

--- src/algorithms/generation/kruskal_maze.ts
+++ src/algorithms/generation/kruskal_maze.ts
@@ -3,19 +3,37 @@ import { getDirection, updateWall } from "../../utils/algorithms.utils";
 
 const DEFAULT_WALL = "0000";
 
+/**
+ * Generate a maze using Kruskal's algorithm.
+ *
+ * @param {number} rows - The number of rows in the maze.
+ * @param {number} columns - The number of columns in the maze.
+ * @returns {[Maze, Coordinate[]]} - A tuple containing the generated maze and an empty trail.
+ */
 export const createMazeKruskal = (rows: number, columns: number): [Maze, Coordinate[]] => {
 	const maze: Maze = Array(rows).fill([]).map(() => Array(columns).fill(DEFAULT_WALL));
 	const sets: number[] = Array(rows * columns).fill(0).map((_, index) => index);
 	const edges: { source: Coordinate, destination: Coordinate }[] = [];
 
-	// Helper function to find the representative element of a set
+	/**
+     * Helper function to find the representative element of a set with path compression.
+     *
+     * @param {number} index - The index of the element.
+     * @returns {number} - The representative element of the set.
+     */
 	const findSet = (index: number): number => {
 		if (sets[index] !== index) 
 			sets[index] = findSet(sets[index]); // Path compression
 		return sets[index];
 	};
 
-	// Helper function to join two sets
+
+    /**
+     * Helper function to join two sets.
+     *
+     * @param {number} index1 - The index of the first element.
+     * @param {number} index2 - The index of the second element.
+     */
 	const unionSets = (index1: number, index2: number): void => {
 		 const root1 = findSet(index1);
 		 const root2 = findSet(index2);
